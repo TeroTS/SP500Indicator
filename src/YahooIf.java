6930931035ef36dd8d997e52ad84ba4c73062f81
Tero Suhonen <tero.suhonen@luukku.com>
2012-11-05 00:12:38 +0200

    Added Yahoo interface class

---
+++ src/YahooIf.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class YahooIf {
+	
+	/*
+	 * open connection to Yahoo stock download site, returns read buffer
+	 */
+	public BufferedReader openYahooConnection() {
+		  BufferedReader reader = null;
+	      try {
+	    	  URL yahoo = new URL("http://ichart.finance.yahoo.com/table.csv?s=YHOO&a=00&b=1&c=2011&d=00&e=1&f=2012&g=d&ignore=.csv"); 
+	          URLConnection yahooConn = yahoo.openConnection();             
+	          reader = new BufferedReader(new InputStreamReader(yahooConn.getInputStream())); 
+	      } catch(Exception ex) {
+	    	  ex.printStackTrace();
+	      }
+	      return reader;
+	}
+
+}
