aa9f43abfa1d14389b120771718b8be72ece705c
Tero Suhonen <tero.suhonen@luukku.com>
2012-11-06 23:38:27 +0200

    Revert "- Added stock data file read"
    This reverts commit e5b98b2c632beff947151f73ff05532ca709679d.

--- src/DbIf.java
+++ src/DbIf.java
@@ -4,10 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.io.*;
-import java.util.HashMap;
 //import java.net.URL; 
 //import java.net.URLConnection; 
-
 
 public class DBIf {
 	
@@ -48,7 +46,7 @@ public class DBIf {
 	/*
 	 * load stock data and write it to database
 	 */
-    public void writeDB(Connection conn, BufferedReader reader, String name) { //throws SQLException {
+    public void writeDB(Connection conn, BufferedReader reader) throws SQLException {
     	//Connection conn = null;
     	Statement statement = null;
 	    // create a database connection
@@ -65,26 +63,24 @@ public class DBIf {
         			//split data fields
         			String[] dataFields = line.split(",");
         			statement.executeUpdate("insert into stock values(" + "'" + dataFields[0] + "'" + "," +
-        					"'" + name + "'" + "," + dataFields[1] + "," + dataFields[2] + "," + dataFields[3] + "," + dataFields[4] + "," + 
+        					"'YHOO'" + "," + dataFields[1] + "," + dataFields[2] + "," + dataFields[3] + "," + dataFields[4] + "," + 
         					dataFields[5] + "," + dataFields[6] + ")");    
         		}
         	} 
 	    } catch(Exception ex) {
 	    	  ex.printStackTrace();
- 
-    	} finally {
-	    	try {
-	    		statement.close();
-	    	} catch(SQLException e) {
-	    		System.err.println(e.getMessage());
-	    	}
-	    }
+    	//} finally {
+    	//	if (statement != null)
+    	}
+	    statement.close();
+	    //close database connection
+	    //closeDBConnection(conn);
     }
     
     /*
      * read database, one row at a time
      */
-    public void readDB(Connection conn) { //throws SQLException {
+    public void readDB(Connection conn) throws SQLException {
     	Statement statement = null;
     	try {
     		statement = conn.createStatement();
@@ -98,19 +94,15 @@ public class DBIf {
     	}  catch(SQLException e) {
 	    	//connection close failed.
 	    	System.err.println(e.getMessage());    		
-    	} finally {
-	    	try {
-	    		statement.close();
-	    	} catch(SQLException e) {
-	    		System.err.println(e.getMessage());
-	    	}
-	    }
+    	}
+    	statement.close();
+    	//return;   	
     }
     
     /*
      * create table
      */
-	public void createTable(Connection conn) { //throws SQLException {
+	public void createTable(Connection conn) throws SQLException {
 		//if (!(new File(DBname).exists())) {	
 		Statement statement = null;
 		String tableString = "create table stock (date text, ticker string, " +
@@ -125,13 +117,8 @@ public class DBIf {
     		statement.executeUpdate(tableString);
 	    } catch(SQLException e) {
 	    	System.err.println(e.getMessage());
-	    } finally {
-	    	try {
-	    		statement.close();
-	    	} catch(SQLException e) {
-	    		System.err.println(e.getMessage());
-	    	}
-	    }
+	    }	
+    	statement.close();
 	}
 	
 	
@@ -141,71 +128,31 @@ public class DBIf {
 	    // load the sqlite-JDBC driver
 	    Class.forName("org.sqlite.JDBC");
 	    
-	    //load S&P500 stocks (from file) using properties !!!!
-	    
-	    //test stock properties
-	    HashMap<String, String> stockProp = new HashMap<String, String>();
-	    stockProp.put("ticker", "intc");
-	    stockProp.put("fromMonth", "0");
-	    stockProp.put("fromDay", "1");
-	    stockProp.put("fromYear", "2012");
-	    stockProp.put("toMonth", "8");
-	    stockProp.put("toDay", "1");
-	    stockProp.put("toYear", "2012");
-	    stockProp.put("freq", "d");
-	    
-	    
 	    DBIf dBIf = new DBIf();
-	    //YahooIf yahooIf = new YahooIf(stockProp);
+	    YahooIf yahooIf = new YahooIf();
 	    
-	    //BufferedReader bufferedReader = null;
+	    BufferedReader bufferedReader = null;
 	    
+	    //create database
+	    //Connection connection = null;
     	// create a database connection
         Connection connection = dBIf.openDBConnection("sp500.db");
-        //create table
-        dBIf.createTable(connection);
         
-	    try {
-	    	//read stock tickers from file,
-	    	//one ticker per line
-	    	File file = new File("SP500.txt");
-	    	FileReader fileReader = new FileReader(file);
-	    	BufferedReader buffReader = new BufferedReader(fileReader);
-	        
-	    	String line = null;
-	    	while ((line = buffReader.readLine()) != null) {
-	    		//set ticker
-	    		stockProp.put("ticker", line);
-	    		//create new Yahoo connection for every stock
-	    		YahooIf yahooIf = new YahooIf(stockProp);
-	    		//connect Yahoo and read data
-	    		BufferedReader bufferedReader = yahooIf.openYahooConnection();
-		        //write database
-		        dBIf.writeDB(connection, bufferedReader, stockProp.get("ticker"));
-		        //close connection
-		        yahooIf.closeYahooConnection(bufferedReader);
-	    		
-	    	}
-	    	buffReader.close();
-	    } catch(IOException e) {
-	    	e.printStackTrace();
-	    }
-	    	
+	    try
+	    {
 	        //create table
-	        //dBIf.createTable(connection);
+	        dBIf.createTable(connection);
 	        //connect Yahoo
-	        //bufferedReader = yahooIf.openYahooConnection();
+	        bufferedReader = yahooIf.openYahooConnection();
 	        //write database
-	        //dBIf.writeDB(connection, bufferedReader);
-	        
-	        
+	        dBIf.writeDB(connection, bufferedReader);
 	        //read database
 	        dBIf.readDB(connection);
-/*	    } catch(SQLException e) {
+	    } catch(SQLException e) {
 	    	System.err.println(e.getMessage());
 		} finally {	       
 			dBIf.closeDBConnection(connection);
-		} */
+		}
 	    
     }
 }
