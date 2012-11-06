a01c9d16cd5a92a7285d52a5cacee05c71838cfa
Tero Suhonen <tero.suhonen@luukku.com>
2012-11-06 23:42:38 +0200

    Added tock file read, fixed exception handling

--- src/YahooIf.java
+++ src/YahooIf.java
@@ -1,27 +1,62 @@
 /*
  * open connection to Yahoo finance and download the stock data
  */
-import java.io.BufferedReader;
+import java.io.*;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-
+import java.util.HashMap;
+//import java.util.Properties;
 
 public class YahooIf {
+	
+	//Yahoo finance data download URL
+	static final String YAHOO_URL = "http://ichart.finance.yahoo.com/table.csv?";
+	
+	//stock download properties (ticker, start and end year/month/day, day/month/year data) 
+	private HashMap<String, String> stockProp;
+	
+	//constructor
+	public YahooIf(HashMap<String, String> hashMap) {
+		//overwrite all the values in stockProp
+		stockProp = hashMap; //.putAll(hashMap);
+	}
 	
 	/*
 	 * open connection to Yahoo, returns read buffer
 	 */
 	public BufferedReader openYahooConnection() {
 		  BufferedReader reader = null;
+		  
 	      try {
-	    	  URL yahoo = new URL("http://ichart.finance.yahoo.com/table.csv?s=YHOO&a=00&b=1&c=2011&d=00&e=1&f=2012&g=d&ignore=.csv"); 
+	    	  URL yahoo = new URL(YAHOO_URL + 
+	    			  			  "s=" + stockProp.get("ticker") + 
+	    			  			  "&d=" + stockProp.get("toMonth") + 
+	    			  	          "&e=" + stockProp.get("toDay") + 
+	    			  		      "&f=" + stockProp.get("toYear") + 	    			  			  
+	    			  			  "&g=" + stockProp.get("freq") +
+	    			  			  "&a=" + stockProp.get("fromMonth") + 
+	    			  			  "&b=" + stockProp.get("fromDay") + 
+	    			  			  "&c=" + stockProp.get("fromYear")); 
+	    	  
 	          URLConnection yahooConn = yahoo.openConnection();             
 	          reader = new BufferedReader(new InputStreamReader(yahooConn.getInputStream())); 
-	      } catch(Exception ex) {
-	    	  ex.printStackTrace();
+	      } catch(Exception e) {
+	    	  e.printStackTrace();
 	      }
 	      return reader;
 	}
-
+	
+	/*
+	 * close yahoo data reader
+	 */
+	public void closeYahooConnection(BufferedReader reader) {
+		try {
+			if (reader != null)
+				reader.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
